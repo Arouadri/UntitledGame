b95cecf1130926533a572b4e6097392e3c8c0e3d
Arouadri <arocaadrian@gmail.com>
2018-08-24 19:59:25 +0200

    Creating camera script

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform[] m_player;
+    public float m_dampTime = 0.2f;
+    private Vector3 m_velocity;
+    private int m_currentPlayer = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown("space")) //TODO: swap the if for actions points = 0 or player or enemy pass his turn 
+        {
+            m_currentPlayer = m_currentPlayer + 1 >= m_player.Length ? 0 : m_currentPlayer + 1;
+        }
+
+       transform.position = Vector3.SmoothDamp(transform.position, m_player[m_currentPlayer].position, ref m_velocity, m_dampTime);
+        
+
+    }
+}
