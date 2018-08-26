c0264fb84eb3ed8c3b3cc96d2931081df104f1b5
Amanda Oliver <amandaolivercalero@gmail.com>
2018-08-26 12:59:21 +0200

    Added Player Movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private LinkedList<Player> m_Players = new LinkedList<Player>();
+    private Player m_CurrentPlayer;
+
+	// Use this for initialization
+	void Start () {
+        Player[] players = FindObjectsOfType<Player>();
+        foreach (Player pl in players) {
+            m_Players.AddLast(pl);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LinkedListNode<Player> current = m_Players.First;
+        current = current.Next;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public enum E_GANG { ALLIES, ENEMIES };
+    protected E_GANG e_gang;
+    protected int m_level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
